e27a97cfccc7bb5b063e9f66b9a3d6ba7776e07c
Saher Scafi <95544378+sscafi@users.noreply.github.com>
2024-06-30 01:20:34 -0400

    Update main.py

--- Jarvis/main.py
+++ Jarvis/main.py
@@ -1,139 +1,136 @@
-import pyttsx3 #pip install pyttsx3
-import speech_recognition as sr #pip install speechRecogniion
-import datetime
-import wikipedia # pip install wikipedia
-import webbrowser
-import os
-import smtplib
-import subprocess
+Here's the code with added comments explaining each line:
 
-fire_fox = 'C:\Program Files\Mozilla Firefox\firefox.exe'
-webbrowser.register('firefox' , None)
-print("Initializing Jarvis")
-MASTER = "Sir"
+```python
+# Importing necessary libraries
+import pyttsx3  # pip install pyttsx3 - Library for text to speech conversion
+import speech_recognition as sr  # pip install speechRecognition - Library for speech recognition
+import datetime  # Library to handle date and time
+import wikipedia  # pip install wikipedia - Library to fetch data from Wikipedia
+import webbrowser  # Library to open web browsers
+import os  # Library to interact with the operating system
+import smtplib  # Library to send emails
+import subprocess  # Library to handle subprocesses
 
+# Defining the path to Firefox browser
+fire_fox = 'C:\\Program Files\\Mozilla Firefox\\firefox.exe'
+# Registering Firefox as the web browser
+webbrowser.register('firefox', None)
+print("Initializing Jarvis")  # Initialization message
+MASTER = "Sir"  # Setting a variable for user address
+
+# Initializing the text to speech engine
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
-engine.setProperty('voice', voices[0].id)
+engine.setProperty('voice', voices[0].id)  # Setting the voice to the first available voice
 
-
-# speak function will speak the string which is passed to it
+# Function to make the engine speak the provided text
 def speak(text):
     engine.say(text)
     engine.runAndWait()
-    
 
-#this function will wish you as per the current time
+# Function to wish the user based on the current time
 def wishMe():
-    hour = int(datetime.datetime.now().hour)
-    
-
-    if hour >= 0 and hour < 12: 
-        speak("Good Morning" + MASTER)
-
+    hour = int(datetime.datetime.now().hour)  # Getting the current hour
+    if hour >= 0 and hour < 12:
+        speak("Good Morning " + MASTER)
     elif hour >= 12 and hour < 18:
-        speak("Good Afternoon" + MASTER)
-
+        speak("Good Afternoon " + MASTER)
     else:
-        speak("Good Evening" + MASTER)
+        speak("Good Evening " + MASTER)
+    speak("How may I help you ......")
 
-    speak(" How may i help you ......")
-
+# Function to take voice commands from the user
 def takeCommand():
-    r = sr.Recognizer()
+    r = sr.Recognizer()  # Initializing the recognizer
     with sr.Microphone() as source:
-        print("Listening.....")
-        audio = r.listen(source)
-    
+        print("Listening.....")  # Indicating that the system is listening
+        audio = r.listen(source)  # Listening to the source
     try:
-        print("Recognizing.....")
-        query = r.recognize_google(audio, language = 'en-GB')
-        print(f"user said: {query}\n")
-
+        print("Recognizing.....")  # Indicating that the system is recognizing
+        query = r.recognize_google(audio, language='en-GB')  # Recognizing speech using Google
+        print(f"user said: {query}\n")  # Printing the recognized text
     except Exception as e:
-        print("Please say that again" + MASTER)
+        print("Please say that again " + MASTER)  # Asking the user to repeat if recognition fails
         query = None
-    return query
+    return query  # Returning the recognized text
 
+# Function to send an email (incomplete implementation)
 def sendEmail():
-    server = smtplib.SMTP('smtp.gmail.com', 587)
-    server.ehlo()
-    server.starttls()
-    server.login('youremail@gmail.com', 'password')
-    server.sendmail
+    server = smtplib.SMTP('smtp.gmail.com', 587)  # Setting up the SMTP server
+    server.ehlo()  # Sending the EHLO command
+    server.starttls()  # Starting TLS for security
+    server.login('youremail@gmail.com', 'password')  # Logging into the email account
+    server.sendmail  # Placeholder for sending the email
 
+# Main function to execute the assistant
 def main():
-# will take command from the microphone
-    speak("Initializing jarvis....")
-    wishMe()
-    query=takeCommand()
-    
+    speak("Initializing Jarvis....")  # Announcing initialization
+    wishMe()  # Calling the wishMe function
+    query = takeCommand()  # Taking the user's command
 
-# logic for executing tasks per query , coding the logic here
-    if 'wikipedia' in query.lower(): 
-        speak('Searching Wikipedia...')
-        query = query.replace("wikipedia", "")
-        results = wikipedia.summary(query, sentences =2)
-        print(results)
-        speak (results) 
+    # Handling different commands
+    if 'wikipedia' in query.lower():
+        speak('Searching Wikipedia...')  # Announcing the search
+        query = query.replace("wikipedia", "")  # Removing 'wikipedia' from the query
+        results = wikipedia.summary(query, sentences=2)  # Fetching summary from Wikipedia
+        print(results)  # Printing the results
+        speak(results)  # Speaking the results
 
     elif 'open youtube' in query.lower():
         webbrowser.register('firefox',
-	    None,
-	    webbrowser.BackgroundBrowser("C://Program Files//Mozilla Firefox//firefox.exe"))
-        webbrowser.get('firefox').open_new_tab("youtube.com") 
+                            None,
+                            webbrowser.BackgroundBrowser("C://Program Files//Mozilla Firefox//firefox.exe"))
+        webbrowser.get('firefox').open_new_tab("youtube.com")  # Opening YouTube
 
-    elif 'open facebook' in query.lower(): #needs work
-    
+    elif 'open facebook' in query.lower():
         url = "facebook.com"
-        webbrowser.open( url) 
+        webbrowser.open(url)  # Opening Facebook
 
     elif 'open anime' in query.lower():
         webbrowser.register('firefox',
-	    None,
-	    webbrowser.BackgroundBrowser("C://Program Files//Mozilla Firefox//firefox.exe"))
-        webbrowser.get('firefox').open_new_tab("www3.gogoanime.cm") 
+                            None,
+                            webbrowser.BackgroundBrowser("C://Program Files//Mozilla Firefox//firefox.exe"))
+        webbrowser.get('firefox').open_new_tab("www3.gogoanime.cm")  # Opening Gogoanime
 
     elif 'open vumo' in query.lower():
         webbrowser.register('firefox',
-	    None,
-	    webbrowser.BackgroundBrowser("C://Program Files//Mozilla Firefox//firefox.exe"))
-        webbrowser.get('firefox').open_new_tab("vumoo.to")
+                            None,
+                            webbrowser.BackgroundBrowser("C://Program Files//Mozilla Firefox//firefox.exe"))
+        webbrowser.get('firefox').open_new_tab("vumoo.to")  # Opening Vumoo
 
     elif 'open songster' in query.lower():
         webbrowser.register('firefox',
-	None,
-	webbrowser.BackgroundBrowser("C://Program Files//Mozilla Firefox//firefox.exe"))
-        webbrowser.get('firefox').open_new_tab("songsterr.com")
+                            None,
+                            webbrowser.BackgroundBrowser("C://Program Files//Mozilla Firefox//firefox.exe"))
+        webbrowser.get('firefox').open_new_tab("songsterr.com")  # Opening Songsterr
 
     elif 'open coding' in query.lower():
         webbrowser.register('firefox',
-	    None,
-	    webbrowser.BackgroundBrowser("C://Program Files//Mozilla Firefox//firefox.exe"))
-        webbrowser.get('firefox').open_new_tab("w3schools.com")
+                            None,
+                            webbrowser.BackgroundBrowser("C://Program Files//Mozilla Firefox//firefox.exe"))
+        webbrowser.get('firefox').open_new_tab("w3schools.com")  # Opening W3Schools
 
     elif 'open notion' in query.lower():
-        os.startfile("C:\\Users\\saher\\AppData\\Local\\Programs\\Notion\\Notion.exe")
- 
+        os.startfile("C:\\Users\\saher\\AppData\\Local\\Programs\\Notion\\Notion.exe")  # Opening Notion
 
-    elif ' play music' in query.lower():
-        songs = os.listdir("")
+    elif 'play music' in query.lower():
+        songs = os.listdir("")  # Listing all songs in the directory
 
-    elif ' the time' in query.lower():
-        strTime = datetime.datetime.now().strftime("%H:%M:%S")
-        speak(f"{MASTER} THE TIME IS {strTime}")
-   
+    elif 'the time' in query.lower():
+        strTime = datetime.datetime.now().strftime("%H:%M:%S")  # Getting the current time
+        speak(f"{MASTER} THE TIME IS {strTime}")  # Speaking the current time
+
     elif 'open visual studio' in query.lower():
-   
-        os.startfile("C:\\Users\\saher\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe")
+        os.startfile("C:\\Users\\saher\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe")  # Opening Visual Studio Code
 
     elif 'open spotify' in query.lower():
-        os.startfile("spotify")
+        os.startfile("spotify")  # Opening Spotify
 
     elif 'open calculator' in query.lower():
-        os.startfile("calculator")
+        os.startfile("calculator")  # Opening Calculator
 
     elif 'open mail' in query.lower():
-        os.startfile("Mail")
+        os.startfile("Mail")  # Opening Mail
 
-main()
+main()  # Calling the main function to start the assistant
+```
