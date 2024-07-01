e0904346a0c05a8037d6ebc872d8849c2914700e
Saher Scafi <95544378+sscafi@users.noreply.github.com>
2024-07-01 02:28:53 -0400

    Update PDF_TO_WORD.py

--- PDF_TO_WORD.py
+++ PDF_TO_WORD.py
@@ -1,8 +1,14 @@
 from pdf2docx import Converter
 
-pdf_file = "your_pdf_file.pdf"
-docx_file = "your_docx_file.docx"
+# Define input and output file paths
+pdf_file = "your_pdf_file.pdf"    # Path to the PDF file to convert
+docx_file = "your_docx_file.docx" # Desired path for the output DOCX file
 
+# Initialize a Converter object with the input PDF file
 converter = Converter(pdf_file)
+
+# Convert the PDF file to DOCX format
 converter.convert(docx_file)
+
+# Close the Converter object to release resources
 converter.close()
